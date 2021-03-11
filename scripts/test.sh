f8c3988847c5f33b506d1dbb2288204fd2a9a1d5
xs75 <xs75@duke.edu>
2021-03-10 22:33:54 -0500

    try again

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gradle build || exit 1
-gradle cloverAggregateReports || exit 1
+./gradlew build || exit 1
+./gradlew cloverAggregateReports || exit 1
 scripts/coverage_summary.sh
 ls -l /
 ls -l /coverage-out/
