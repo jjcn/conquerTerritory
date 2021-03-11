54b8151f4055de1d9dea56d43cbdd6f901ada989
xs75 <xs75@duke.edu>
2021-03-11 01:05:23 -0500

    update the packge name

--- src/main/java/edu/duke/group4/RISK/Order.java
+++
@@ -1,8 +0,0 @@
-package edu.duke.group4.RISK;
-
-public interface Order {
-  public String getSrcName();
-  public String getDesName();
-  public Troop getActTroop();
-  public Character getActionName();
-}
--- src/main/java/edu/duke/group4/RISK/WorldView.java
+++
@@ -1,5 +0,0 @@
-package edu.duke.group4.RISK;
-
-public interface WorldView {
-  void displayWorld(World toDisplay);
-}
