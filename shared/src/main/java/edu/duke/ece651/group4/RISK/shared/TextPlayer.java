13b3c3cf98b850b68319f3434c649e8a772d90db
WenxinXu <wx65@duke.edu>
2021-03-12 11:14:08 +0800

    try idea

--- shared/src/main/java/edu/duke/ece651/group4/RISK/shared/TextPlayer.java
+++ shared/src/main/java/edu/duke/ece651/group4/RISK/shared/TextPlayer.java
@@ -3,28 +3,28 @@ package edu.duke.ece651.group4.RISK.shared;
 import java.io.IOException;
 
 public class TextPlayer implements Player {
-  private String playername;
-  final PrintStream out;
-  final Reader inputReader;
+    private String playername;
+    final PrintStream out;
+    final Reader inputReader;
 
-  public TextPlayer(Reader inputSource, PrintStream out)throws IOException{
-    this.playername = getName();
-    this.out = out;
-    this.inputReader = inputSource;
-  }
+    public TextPlayer(Reader inputSource, PrintStream out) throws IOException {
+        this.playername = getName();
+        this.out = out;
+    }
 
-  public Order doOneAction()throws IOException{
-    return null;
-  }
+    public Order doOneAction() throws IOException {
+        return null;
+    }
 
-  public int chooseTerritory(HashMap<int,Territory> map) throws IOException {
-    return 0;
-  }
-  /**
-@return name of a player.
-   **/
-  public String getName(){
-    return playername;
-  }
+    public int chooseTerritory(HashMap<int, Territory> map) throws IOException {
+        return 0;
+    }
+
+    /**
+     * @return name of a player.
+     **/
+    public String getName() {
+        return playername;
+    }
 
 }
