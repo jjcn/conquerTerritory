a43874810cb8fcf7d0ea39bc11f99cc38e2a0ea6
zian <gengz@lafayette.edu>
2021-03-12 02:31:03 -0500

    territory finish

---
+++ shared/src/test/java/edu/duke/ece651/group4/RISK/shared/TerritoryTest.java
@@ -0,0 +1,50 @@
+package edu.duke.ece651.group4.RISK.shared;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TerritoryTest {
+
+    @Test
+    void Test_sendTroop() {
+        Territory test= new Territory("test");
+        test.initializeTerritory(5,null);
+        test.sendInTroop(new Troop(5,null));
+        assertEquals(test.checkPopulation(),10);
+        test.sendOutTroop(new Troop(4,null));
+        assertEquals(test.checkPopulation(),6);
+        test.sendInEnemyTroop(new Troop(4,null));
+        assertEquals(test.checkPopulation(),6);
+    }
+
+    @Test
+    void Test_addUnit() {
+        Territory test= new Territory("test");
+        test.initializeTerritory(5,null);
+        test.addUnit(5);
+        assertEquals(test.checkPopulation(),10);
+    }
+
+    @Test
+    void Test_initializeTerritory() {
+        Territory test= new Territory("test");
+        test.initializeTerritory(5,null);
+        assertEquals(test.checkPopulation(),5);
+    }
+
+//    @Test
+//    void Test_doBattles() throws IOException {
+//        Player p1=new TextPlayer(null,"p1",null,null);
+//        Territory test= new Territory("test",p1,2,new Random(0));
+//        test.sendInEnemyTroop(new Troop(2,new TextPlayer(null,"p2",null,null)));
+//        test.sendInEnemyTroop(new Troop(2,new TextPlayer(null,"p3",null,null)));
+//
+//        assertEquals(test.checkPopulation(),1);
+//        assertEquals(test.getOwner(),p1);
+//
+//    }
+}
