e148f8591d57f88229d685f394fb060d8febe264
Shengxi Jin <sj320@duke.edu>
2021-03-17 20:42:04 -0400

    Shuffler coverage

--- shared/src/test/java/edu/duke/ece651/group4/RISK/shared/ShufflerTest.java
+++ shared/src/test/java/edu/duke/ece651/group4/RISK/shared/ShufflerTest.java
@@ -1,17 +1,19 @@
 package edu.duke.ece651.group4.RISK.shared;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import org.junit.jupiter.api.Test;
 
 public class ShufflerTest {
-    @Test
-    public void visualDisplay() {
-        int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-        Shuffler shuffler = new Shuffler(new Random(0));
 
+    @Test
+    public void testShuffle() { // a visual test
+        int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        Shuffler shuffler = new Shuffler();
         shuffler.shuffle(arr);
 
         StringBuilder sb = new StringBuilder();
@@ -20,7 +22,27 @@ public class ShufflerTest {
             sb.append(delim + i);
             delim = ", ";
         }
-        System.out.print(sb);
+        System.out.println(sb);
+    }
+
+    @Test
+    public void testShuffleWithSeed() {
+        int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        int arr1[] = Arrays.copyOf(arr, arr.length);
+        Shuffler shuffler = new Shuffler(new Random(0));
+        Shuffler shuffler1 = new Shuffler(new Random(0));
+        shuffler.shuffle(arr);
+        shuffler1.shuffle(arr1);
+
+        StringBuilder sb = new StringBuilder();
+        String delim = "";
+        for (int i : arr) {
+            sb.append(delim + i);
+            delim = ", ";
+        }
+        System.out.println(sb);
+
+        assertArrayEquals(arr, arr1);
     }
 
 }
